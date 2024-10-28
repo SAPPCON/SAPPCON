db0ad0f14548d3325ad38900ba5eeb445f566515
FacundoFalchini <facundofaclchini@gmail.com>
2024-10-28 12:35:21 -0300

    Frontend - Fix import

--- Frontend/src/pages/new-password/[token].js
+++ Frontend/src/pages/new-password/[token].js
@@ -1,13 +1,10 @@
 import React, { Fragment, useContext, useEffect } from "react";
 import Head from "next/head";
-import Loader from "@/components/UI/Loader";
 import { useRouter } from "next/router";
-import AuthenticationContext from "@/store/AuthenticationContext";
-import NewPassword from "@/components/Authentication/newPassword";
+import NewPassword from "@/components/Authentication/NewPassword";
 
 function NamePage() {
   const router = useRouter();
-  const authenticationContext = useContext(AuthenticationContext);
   const { token } = router.query;
 
   return (
