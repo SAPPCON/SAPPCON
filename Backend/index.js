5a7cdb8e38ba285903f0565e512f6fdf5244f99d
FacundoFalchini <facundofaclchini@gmail.com>
2024-10-29 16:44:16 -0300

    Backend - Add header to cors and fix maxLenght of measureUnit

--- Backend/index.js
+++ Backend/index.js
@@ -1,32 +1,31 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import dotenv from 'dotenv'
-import mongoose from 'mongoose';
-import connectDB from './database/mongodb.js'
-import userRoutes from './routes/user.js'
-import categoryRoutes from './routes/category.js'
-import measureUnitRoutes from './routes/measureUnit.js'
-import customerRoutes from './routes/customer.js'
-import serviceRoutes from './routes/service.js'
-import buildingRoutes from './routes/building.js';
-import budgetRoutes from './routes/budget.js';
-import reporterRoutes from './routes/reporter.js';
-import statsRoutes from './routes/stats.js';
-import imageRoutes from './routes/image.js';
-import helmet from 'helmet';
-import cors from 'cors'
-import fs from 'fs'
-import https from 'https'
-
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import connectDB from "./database/mongodb.js";
+import userRoutes from "./routes/user.js";
+import categoryRoutes from "./routes/category.js";
+import measureUnitRoutes from "./routes/measureUnit.js";
+import customerRoutes from "./routes/customer.js";
+import serviceRoutes from "./routes/service.js";
+import buildingRoutes from "./routes/building.js";
+import budgetRoutes from "./routes/budget.js";
+import reporterRoutes from "./routes/reporter.js";
+import statsRoutes from "./routes/stats.js";
+import imageRoutes from "./routes/image.js";
+import helmet from "helmet";
+import cors from "cors";
+import fs from "fs";
+import https from "https";
 
 /* ---- CONFIGURATION ---- */
-dotenv.config()
+dotenv.config();
 const app = express();
 mongoose.set("strictQuery", true);
-app.use(express.json())
-app.use(bodyParser.json())
-app.use(helmet())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: true }));
 const allowedOrigins = [
   "http://localhost:3000",
   "https://sappcon.site",
@@ -37,21 +36,21 @@ app.use(
     origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+    exposedHeaders: ["Content-Disposition"], //Permite al front acceder a Content Disposition en la respuesta
   })
 );
 
 /* ---- ROUTES ---- */
-app.use('/user', userRoutes)
-app.use('/category', categoryRoutes)
-app.use('/measureUnit', measureUnitRoutes)
-app.use('/customer', customerRoutes)
-app.use('/service', serviceRoutes)
-app.use('/building', buildingRoutes)
-app.use('/budget', budgetRoutes)
+app.use("/user", userRoutes);
+app.use("/category", categoryRoutes);
+app.use("/measureUnit", measureUnitRoutes);
+app.use("/customer", customerRoutes);
+app.use("/service", serviceRoutes);
+app.use("/building", buildingRoutes);
+app.use("/budget", budgetRoutes);
 app.use("/reporter", reporterRoutes);
 app.use("/stats", statsRoutes);
 app.use("/image", imageRoutes);
-
 
 /* ---- INITIALIZATION ---- */
 const isProduction = process.env.ENV === "production";
@@ -66,22 +65,30 @@ const dbPort = isProduction
 const dbName = isProduction
   ? process.env.PROD_DB_NAME
   : process.env.DEV_DB_NAME;
-const dbUser = isProduction ? process.env.PROD_DB_USER : 'null';
-const dbPass = isProduction ? process.env.PROD_DB_PASS : 'null';
-
-
+const dbUser = isProduction ? process.env.PROD_DB_USER : "null";
+const dbPass = isProduction ? process.env.PROD_DB_PASS : "null";
 
 init(process.env.APP_PORT, dbHost, dbPort, dbName, dbUser, dbPass);
 
 async function init(port, dbHost, dbPort, dbName, dbUser, dbPass) {
   try {
-    connectDB({ host: dbHost, port: dbPort, dbName: dbName, user:dbUser, pass:dbPass});
+    connectDB({
+      host: dbHost,
+      port: dbPort,
+      dbName: dbName,
+      user: dbUser,
+      pass: dbPass,
+    });
     if (isProduction) {
       const options = {
         key: fs.readFileSync("/etc/letsencrypt/live/sappcon.site/privkey.pem"), // Ruta clave privada
-        cert: fs.readFileSync("/etc/letsencrypt/live/sappcon.site/fullchain.pem"), // Ruta certificado
+        cert: fs.readFileSync(
+          "/etc/letsencrypt/live/sappcon.site/fullchain.pem"
+        ), // Ruta certificado
       };
-      https.createServer(options, app).listen(port, () => { console.log(`Server is running on port ${port}`)});
+      https.createServer(options, app).listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+      });
     } else {
       app.listen(port, () => console.log(`Server is running on port ${port}`));
     }
--- Backend/models/budgetLine.js
+++ Backend/models/budgetLine.js
@@ -3,65 +3,66 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const budgetLineSchema = new Schema(
-{
+  {
     user_id: {
-            type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     budget_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Budget",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Budget",
+      required: true,
     },
     line_no: {
-        type: Number,
+      type: Number,
     },
     service_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Service",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Service",
+      required: true,
     },
     service_name: {
-        type: String,
-        required: true,
-        maxlength: 250,
+      type: String,
+      required: true,
+      maxlength: 250,
     },
     measure_unit_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "MeasureUnit",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "MeasureUnit",
+      required: true,
     },
     measure_unit_name: {
-        type: String,
-        required: true,
-        maxlength: 50,
+      type: String,
+      required: true,
+      maxlength: 250,
     },
     quantity: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     price: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     amount: {
-        type: Number,
-        required: true,
-    }
-},
-{
+      type: Number,
+      required: true,
+    },
+  },
+  {
     timestamps: true,
-}
+  }
 );
 
-budgetLineSchema.pre('save', async function (next) {
-    if (this.isNew) {
-        const count = await BudgetLine.countDocuments({ budget_id: this.budget_id });
-        this.line_no = count + 1;
-    }
+budgetLineSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    const count = await BudgetLine.countDocuments({
+      budget_id: this.budget_id,
+    });
+    this.line_no = count + 1;
+  }
 });
 
 const BudgetLine = mongoose.model("BudgetLine", budgetLineSchema);
 export default BudgetLine;
-
